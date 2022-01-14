d2908058b2233e293020b03818aa7a2f5902a22a
Christian Roese <croese@8thlight.com>
2022-01-14 09:18:56 -0600

    adds more parser tests and code for checking version and comments

--- skeleton/parser.py
+++ skeleton/parser.py
@@ -1,4 +1,25 @@
+from _pytest.python_api import raises
 from skeleton.commands import CreateFileCommand
+
+
+import re
+
+
+class MissingVersionError(Exception):
+    pass
+
+
+VERSION_MATCHER = re.compile(r"#\s*skel\d")
+
+
+def create_parser(stream):
+    first_line = stream.readline().strip()
+
+    if not VERSION_MATCHER.match(first_line):
+        raise MissingVersionError()
+
+    # no parser choice logic needed right now
+    return SkeletonParserV1(stream)
 
 
 class SkeletonParserV1:
@@ -8,6 +29,8 @@ class SkeletonParserV1:
     def parse(self):
         commands = []
         for line in self._stream:
+            if line.startswith("#"):
+                continue
             commands.append(CreateFileCommand(line.strip()))
 
         return commands
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,16 +1,43 @@
-from skeleton.parser import SkeletonParserV1
+from skeleton.parser import *
 from io import StringIO
 from skeleton.commands import *
+import pytest
 
 
-def test_parser_returns_commands_for_basic_file_creation():
-    file = StringIO("foo.txt\nbar")
-    p = SkeletonParserV1(file)
+class TestParserCreation:
+    def test_create_parser_throws_when_missing_version(self):
+        file = StringIO("foo.txt\nbar")
 
-    commands = p.parse()
+        with pytest.raises(MissingVersionError):
+            p = create_parser(file)
 
-    assert len(commands) == 2
-    assert isinstance(commands[0], CreateFileCommand)
-    assert isinstance(commands[1], CreateFileCommand)
-    assert commands[0].filename == "foo.txt"
-    assert commands[1].filename == "bar"
+    def test_create_parser_consumes_version_and_returns_parser(self):
+        # just testing for the only version, for now - expand if more versions are added
+        file = StringIO("# skel1\nfoo.txt\nbar")
+
+        p = create_parser(file)
+
+        assert isinstance(p, SkeletonParserV1)
+        assert file.readline() == "foo.txt\n"
+
+
+class TestV1Parser:
+    def test_parser_returns_commands_for_basic_file_creation(self):
+        file = StringIO("foo.txt\nbar")
+        p = SkeletonParserV1(file)
+
+        commands = p.parse()
+
+        assert len(commands) == 2
+        assert isinstance(commands[0], CreateFileCommand)
+        assert isinstance(commands[1], CreateFileCommand)
+        assert commands[0].filename == "foo.txt"
+        assert commands[1].filename == "bar"
+
+    def test_parser_skips_comments(self):
+        file = StringIO("foo.txt\n# a comment\nbar")
+        p = SkeletonParserV1(file)
+
+        commands = p.parse()
+
+        assert len(commands) == 2
